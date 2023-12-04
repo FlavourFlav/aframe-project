b8d4a6c74e367d254a915c76c51ff88aaa130e45
TobX12 <tobias.renner@t-online.de>
2023-12-04 23:26:53 +0100

    Added Frontend Server

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react'
+
+function App() {
+  const [backendData, setBackendData] = useState([{}])
+  useEffect(() => {
+    fetch("/api")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Server response wasn\'t OK');
+        }
+      })
+      .then(data => {
+        setBackendData(data);
+      })
+      .catch(error => {
+        console.error('There was a problem with your fetch operation:', error);
+      });
+  }, []);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
