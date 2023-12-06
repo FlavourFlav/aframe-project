0a12a2ecef014b79f3aac4d4e2b5b9fdd9e8d633
FlavourFlav <f.criscione@campus.lmu.de>
2023-12-06 20:45:00 +0100

    Code cleanup, enabled HTTPS for react app & attempt to include aframe

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, {useEffect, useState} from 'react'
+import 'aframe';
+import 'aframe-ar';
+
 
 function App() {
+
   const [backendData, setBackendData] = useState([{}])
   useEffect(() => {
     fetch("https://localhost:8443/data")
@@ -13,15 +17,41 @@ function App() {
       })
       .then(data => {
         setBackendData(data);
+        console.log(data);
       })
       .catch(error => {
         console.error('There was a problem with your fetch operation:', error);
       });
   }, []);
+
   return (
-    <div>
-      
-    </div>
+    <a-scene
+      vr-mode-ui="enabled: false"
+      embedded
+      arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: true;"
+    >
+      {backendData.map((location, index) => {
+        return (
+          <a-box
+            gps-entity-place={`latitude: ${location.latitude}; longitude: ${location.longitude};`}
+            name="location"
+            key={index}
+            material="color: red;"
+            scale="10 10 10"
+            position="0 0 0"
+          >
+            <a-text
+              value={location.adresse}
+              color="black"
+              align="center"
+              position="0 0 0"
+            ></a-text>
+          </a-box>
+        )
+      }
+      )}
+    <a-camera gps-camera="maxDistance: 20; minDistance: 5; simulateLatitude: 48.116937; simulateLongitude: 11.539696;" rotation-reader> </a-camera>
+    </a-scene>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const options = {
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['https://localhost:3000'];
 app.use(cors({
   origin: function(origin, callback){
     if (!origin) {
